3b494eb1cae6d43985bfa2102d1f7e33d41cdb33
Tor <torberge@outlook.com>
2025-07-05 11:15:24 +0200

    bmp

--- Casks/speki.rb
+++ Casks/speki.rb
@@ -1,6 +1,6 @@
 cask "speki" do
-  version "0.1.0"
-  sha256 "01089f93a0807464433bb19079d6066ec7ed41e6d5a647715a3f7420174fa251"
+  version "0.1.1"
+  sha256 "7e3a26a12242c07524c5e48bca6f360b2b06db094ec85d4906474cab3ebbfd9b"
 
   url "https://github.com/tbs1996/spekispace/releases/download/v#{version}/speki.dmg",
       verified: "github.com/tbs1996/spekispace/"
